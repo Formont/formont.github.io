7d6327eb0faab4c5c000ed9f37c5a1da23202a78
Kirill <139577121+Formont@users.noreply.github.com>
2024-03-23 19:32:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let tg = window.Telegram.WebApp;
 
-alert(tg.ThemeParams.bg_color);
+HeaderText= document.getElementById("header_news")
+if (tg.colorScheme == "light"){
+    HeaderText.style.color == "black";
+}
+else if (tg.colorScheme == "dark"){
+    HeaderText.style.color == "white";
+}
 tg.expand();
 
 tg.MainButton.textColor = '#FFFFFF';
