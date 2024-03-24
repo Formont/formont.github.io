0ac5a5404cb6045da4a284ea76d6a660f8bf13b9
Kirill <139577121+Formont@users.noreply.github.com>
2024-03-24 10:43:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ let tg = window.Telegram.WebApp;
 
 if (tg.colorScheme == "light"){
     document.getElementById("header_container").style.color = "black";
+    document.getElementsByClassName("scroller_text").style.color = "black"
 }
 else if (tg.colorScheme == "dark"){
     document.getElementById("header_container").style.color = "white";
+    document.getElementsByClassName("scroller_text").style.color = "white"
 }
 tg.expand();
 
